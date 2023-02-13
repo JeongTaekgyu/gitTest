cdfda55a2535ef4856f5b9dd047f7a3633f3e6a8
Jtaekgyu <tk.jeong@bitnine.net>
2023-02-14 08:29:00 +0900

    branch2 test commit

--- src/main/java/com/example/gittest1/controller/Branch2Controller.java
+++ src/main/java/com/example/gittest1/controller/Branch2Controller.java
@@ -3,7 +3,9 @@ package com.example.gittest1.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Entity;
+
 @RestController
 @RequiredArgsConstructor
-public class Test2Controller {
+public class Branch2Controller {
 }
