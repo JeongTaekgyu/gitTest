ed6bfd9e923edb71e2b0acb6d2854097939304f5
JeongTaekgyu <taekgyu0602@gmail.com>
2023-02-15 10:13:22 +0900

    git config 정보 수정1

---
+++ src/main/java/com/example/gittest1/controller/config/GitConfig.java
@@ -0,0 +1,11 @@
+package com.example.gittest1.controller.config;
+
+public class GitConfig {
+    // git config user.name "유저네임"
+    // git config user.email "이메일"
+    // 이렇게 변경해서
+    // git config user.name
+    // git config user.email
+    // 이렇게 두 개 를 확인하면 제대로 바뀐게 반영되는데
+    // git config --list 에서 user.name과 user.email을 보면 변경이 되지 않았다..
+}
